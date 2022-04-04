8d74a672167d5c631a32f775cb751064b6c2e0f1
IrumiDesu7 <ikalam89@gmail.com>
2022-04-04 13:06:20 +0800

    added result and score

--- app.js
+++ app.js
@@ -1,36 +1,55 @@
+const playerSelectionBtns = document.querySelectorAll('.btn');
+const result = document.querySelector('.result');
+const playerScore = document.querySelector('#yourScore');
+const botScore = document.querySelector('#botScore');
+let arr = [0, 0];
+let yourScore = arr[0];
+let computerScore = arr[1];
+
 function computerPlay() {
   let computerWeapons = ['rock', 'paper', 'scissors'];
   return computerWeapons[Math.floor(Math.random() * computerWeapons.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
-  let score = 0;
+  let arr = ['AW, YOU LOSE', 0];
+
   if (playerSelection === computerSelection) {
-    score;
+    arr[0] = 'DRAW';
   } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-    score++;
+    arr[0] = 'YOU WIN';
+    arr[1]++;
   } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-    score++;
-  } else if (playerSelection === 'scissor' && computerSelection === 'paper') {
-    score++;
+    arr[0] = 'YOU WIN';
+    arr[1]++;
+  } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+    arr[0] = 'YOU WIN';
+    arr[1]++;
   }
-  return score;
+  return arr;
 }
 
-function game() {
-  let total = 0;
-  console.log(
-    'if your score is higher or equal to 3, you win. otherwise you lose'
-  );
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = prompt('rock paper scissors', 'rock');
-    total += playRound(playerSelection, computerPlay());
-  }
-  if (total > 3) {
-    return `your score is : ${total}/5. you WIN`;
-  } else {
-    return `your score is : ${total}/5. you LOSE`;
-  }
-}
-
-console.log(game());
+playerSelectionBtns.forEach((e) =>
+  e.addEventListener('click', function () {
+    const playerSelection = e.textContent.toLowerCase();
+    const computerSelection = computerPlay();
+    const resultText = playRound(playerSelection, computerSelection);
+    result.textContent = resultText[0];
+    if (result.textContent === 'AW, YOU LOSE') {
+      yourScore++;
+    } else if (result.textContent === 'YOU WIN') {
+      computerScore++;
+    }
+    playerScore.textContent = yourScore;
+    botScore.textContent = computerScore;
+    if (yourScore === 5) {
+      result.textContent = 'YOU WIN THIS MATCH!';
+      computerScore = 0;
+      yourScore = 0;
+    } else if (computerScore === 5) {
+      result.textContent = 'COMPUTER WIN! WHY U SO NOOB?';
+      computerScore = 0;
+      yourScore = 0;
+    }
+  })
+);
