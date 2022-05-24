3c47bcc0f18c88e3607b18f703368111d4fbf717
IrumiDesu7 <ikalam89@gmail.com>
2022-05-25 01:34:33 +0800

    redone for practice

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const rps = ['rock', 'paper', 'scissors'];
+const buttons = document.querySelectorAll('button');
+const roundWinnerDiv = document.querySelector('.roundWinner');
+const userScoreDiv = document.querySelector('.userScore');
+const computerScoreDiv = document.querySelector('.computerScore');
 let userScore = 0;
 let computerScore = 0;
 let draw = 0;
+let isGameFinished = false;
 
 function computerPlay() {
   return rps[Math.floor(Math.random() * 3)];
@@ -10,51 +15,54 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
   let referee = '';
-  if (playerSelection === computerSelection) {
-    referee = "It's a draw!";
-    draw++;
-  } else if (playerSelection === rps[0] && computerSelection === rps[2]) {
-    referee = `You win! ${playerSelection} beats ${computerSelection} `;
-    userScore++;
-  } else if (playerSelection === rps[1] && computerSelection === rps[0]) {
-    referee = `You win! ${playerSelection} beats ${computerSelection} `;
-    userScore++;
-  } else if (playerSelection === rps[2] && computerSelection === rps[1]) {
-    referee = `You win! ${playerSelection} beats ${computerSelection} `;
-    userScore++;
+  if (!isGameFinished) {
+    if (playerSelection === computerSelection) {
+      referee = "It's a draw!";
+      draw++;
+    } else if (playerSelection === rps[0] && computerSelection === rps[2]) {
+      referee = `You win! ${playerSelection} beats ${computerSelection} `;
+      userScore++;
+    } else if (playerSelection === rps[1] && computerSelection === rps[0]) {
+      referee = `You win! ${playerSelection} beats ${computerSelection} `;
+      userScore++;
+    } else if (playerSelection === rps[2] && computerSelection === rps[1]) {
+      referee = `You win! ${playerSelection} beats ${computerSelection} `;
+      userScore++;
+    } else {
+      referee = `Computer wins! ${computerSelection} beats ${playerSelection} `;
+      computerScore++;
+    }
   } else {
-    referee = `You Lose! ${computerSelection} beats ${playerSelection} `;
-    computerScore++;
+    restartGame();
+    referee = `Choose Your Weapon!`;
   }
+  isGameFinished = false;
   return referee;
 }
 
-function game() {
-  for (let i = 0; i < 5; i++) {
-    let userInput = prompt();
-    playRound(userInput, computerPlay());
-    if (i === 4) {
-      if (userScore > computerScore) {
-        console.log(
-          'You win! Score : ' +
-            userScore +
-            '\nEnemy Score : ' +
-            computerScore +
-            '\ndraw count : ' +
-            draw
-        );
-      } else {
-        console.log(
-          'You lose! Score : ' +
-            userScore +
-            '\nEnemy Score : ' +
-            computerScore +
-            '\ndraw count : ' +
-            draw
-        );
-      }
-    }
+function restartGame() {
+  userScore = 0;
+  computerScore = 0;
+}
+
+function gameWinner(a, b) {
+  if (a === 5) {
+    roundWinnerDiv.textContent = `You win! Click any buttons to rematch`;
+    isGameFinished = true;
+  } else if (b === 5) {
+    roundWinnerDiv.textContent = `Computer wins! Click any buttons to rematch`;
+    isGameFinished = true;
   }
 }
 
-game();
+buttons.forEach((element) => {
+  element.addEventListener('click', function (e) {
+    roundWinnerDiv.textContent = playRound(
+      e.target.textContent,
+      computerPlay()
+    );
+    userScoreDiv.textContent = 'Your Score : ' + userScore;
+    computerScoreDiv.textContent = 'Computer Score : ' + computerScore;
+    gameWinner(userScore, computerScore);
+  });
+});
