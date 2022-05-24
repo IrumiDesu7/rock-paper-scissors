b49eb9611bea5bc7e31e3dd4567183dd7615e598
IrumiDesu7 <ikalam89@gmail.com>
2022-05-25 00:23:39 +0800

    redoing it for practice

--- app.js
+++ app.js
@@ -1,55 +1,60 @@
-const playerSelectionBtns = document.querySelectorAll('.btn');
-const result = document.querySelector('.result');
-const playerScore = document.querySelector('#yourScore');
-const botScore = document.querySelector('#botScore');
-let arr = [0, 0];
-let yourScore = arr[0];
-let computerScore = arr[1];
+const rps = ['rock', 'paper', 'scissors'];
+let userScore = 0;
+let computerScore = 0;
+let draw = 0;
 
 function computerPlay() {
-  let computerWeapons = ['rock', 'paper', 'scissors'];
-  return computerWeapons[Math.floor(Math.random() * computerWeapons.length)];
+  return rps[Math.floor(Math.random() * 3)];
 }
 
 function playRound(playerSelection, computerSelection) {
-  let arr = ['AW, YOU LOSE', 0];
-
+  playerSelection = playerSelection.toLowerCase();
+  let referee = '';
   if (playerSelection === computerSelection) {
-    arr[0] = 'DRAW';
-  } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-    arr[0] = 'YOU WIN';
-    arr[1]++;
-  } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-    arr[0] = 'YOU WIN';
-    arr[1]++;
-  } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-    arr[0] = 'YOU WIN';
-    arr[1]++;
+    referee = "It's a draw!";
+    draw++;
+  } else if (playerSelection === rps[0] && computerSelection === rps[2]) {
+    referee = `You win! ${playerSelection} beats ${computerSelection} `;
+    userScore++;
+  } else if (playerSelection === rps[1] && computerSelection === rps[0]) {
+    referee = `You win! ${playerSelection} beats ${computerSelection} `;
+    userScore++;
+  } else if (playerSelection === rps[2] && computerSelection === rps[1]) {
+    referee = `You win! ${playerSelection} beats ${computerSelection} `;
+    userScore++;
+  } else {
+    referee = `You Lose! ${computerSelection} beats ${playerSelection} `;
+    computerScore++;
   }
-  return arr;
+  return referee;
 }
 
-playerSelectionBtns.forEach((e) =>
-  e.addEventListener('click', function () {
-    const playerSelection = e.textContent.toLowerCase();
-    const computerSelection = computerPlay();
-    const resultText = playRound(playerSelection, computerSelection);
-    result.textContent = resultText[0];
-    if (result.textContent === 'AW, YOU LOSE') {
-      yourScore++;
-    } else if (result.textContent === 'YOU WIN') {
-      computerScore++;
+function game() {
+  for (let i = 0; i < 5; i++) {
+    let userInput = prompt();
+    playRound(userInput, computerPlay());
+    if (i === 4) {
+      if (userScore > computerScore) {
+        console.log(
+          'You win! Score : ' +
+            userScore +
+            '\nEnemy Score : ' +
+            computerScore +
+            '\ndraw count : ' +
+            draw
+        );
+      } else {
+        console.log(
+          'You lose! Score : ' +
+            userScore +
+            '\nEnemy Score : ' +
+            computerScore +
+            '\ndraw count : ' +
+            draw
+        );
+      }
     }
-    playerScore.textContent = yourScore;
-    botScore.textContent = computerScore;
-    if (yourScore === 5) {
-      result.textContent = 'YOU WIN THIS MATCH!';
-      computerScore = 0;
-      yourScore = 0;
-    } else if (computerScore === 5) {
-      result.textContent = 'COMPUTER WIN! WHY U SO NOOB?';
-      computerScore = 0;
-      yourScore = 0;
-    }
-  })
-);
+  }
+}
+
+game();
